534c709061bae8a840ee384ebfade8fab00db459
Eric Gentry <ericsnekbytes@gmail.com>
2024-08-07 14:11:22 -0400

    Revise layout/styling.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,52 @@ import {
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
+import { Checkbox } from '@jupyter/web-components';
+
+// import { Signal } from '@lumino/signaling';
+
+import {
+  Widget
+} from '@lumino/widgets';
+
+class FsspecWidget extends Widget {
+
+  constructor() {
+    super();
+
+    this.node.classList.add('jfsspec-root');
+
+    // let uiToolkit = new DesignSystemProvider();
+    let primaryDivider = document.createElement('div');
+    primaryDivider.classList.add('jfsspec-primarydivider');
+    // uiToolkit.appendChild(primaryDivider);
+
+    let upperArea = document.createElement('div');
+    upperArea.innerText = 'Hello, sample!'
+    upperArea.classList.add('jfsspec-upperarea')
+
+    let hsep = document.createElement('div');
+    hsep.classList.add('jfsspec-hseparator');
+
+    let lowerArea = document.createElement('div');
+    lowerArea.classList.add('jfsspec-lowerarea')
+
+    let resultArea = document.createElement('div');
+    resultArea.classList.add('jfsspec-resultarea')
+    lowerArea.appendChild(resultArea);
+
+    primaryDivider.appendChild(upperArea);
+    primaryDivider.appendChild(hsep);
+    primaryDivider.appendChild(lowerArea);
+
+    let cbox = new Checkbox();
+    cbox.style.marginLeft = '1rem';
+    upperArea.appendChild(cbox);
+
+    this.node.appendChild((primaryDivider));
+  }
+}
+
 /**
  * Initialization data for the jupyterFsspec extension.
  */
@@ -15,6 +61,10 @@ const plugin: JupyterFrontEndPlugin<void> = {
   optional: [ISettingRegistry],
   activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
     console.log('JupyterLab extension jupyterFsspec is activated!');
+
+    let fsspec_widget = new FsspecWidget();
+    fsspec_widget.id = 'jupyterFsspec:widget'
+    app.shell.add(fsspec_widget, 'right');
 
     if (settingRegistry) {
       settingRegistry
