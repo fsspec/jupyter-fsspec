74b63399e3321b828e5e407178f330e2d582af28
RRosio <rosio_reyes@yahoo.com>
2025-02-25 23:10:57 -0800

    update descriptions for models

--- jupyter_fsspec/models.py
+++ jupyter_fsspec/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from enum import Enum
 
@@ -13,20 +13,62 @@ class RequestAction(str, Enum):
 
 
 class BaseRequest(BaseModel):
-    key: str
-    item_path: str
+    """
+    The required information for all Filesystem handler endpoints.
+
+    key: unique
+    item_path: destination path for the acting filesystem
+    """
+
+    key: str = Field(
+        ...,
+        title="Filesystem name",
+        description="Unique identifier given as the filesystem 'name' in the config file",
+    )
+    item_path: str = Field(..., title="Path", description="Acting path in filesystem")
 
 
 class GetRequest(BaseRequest):
-    type: Optional[RequestType] = RequestType.default
+    """
+    GET request specific items.
+
+    type: option to specify type of GET request
+    """
+
+    type: Optional[RequestType] = Field(
+        default=RequestType.default,
+        title="Type of GET request",
+        description="Either a 'range' GET request for file or 'default' for normal GET",
+    )
 
 
 class PostRequest(BaseRequest):
-    content: Optional[str] = None
-    action: Optional[RequestAction] = None
+    """
+    POST request specific items.
+
+    content: content to be created upon request
+    action: move action specified when calling action handler
+    """
+
+    content: Optional[str] = Field(
+        default=None,
+        title="File content or file/directory name",
+        description="Content to be created upon request",
+    )
+    action: Optional[RequestAction] = Field(
+        default=None,
+        title="Move or copy action indicator",
+        description="Specify 'move' action when calling action handler, default treated as copy",
+    )
 
 
 class DeleteRequest(BaseRequest):
+    """
+    Placeholder model for delete request
+
+    No additional information is needed than base request
+    """
+
     pass
 
 
@@ -36,8 +78,30 @@ class Direction(str, Enum):
 
 
 class TransferRequest(BaseModel):
-    key: str
-    destination_key: str
+    """
+    Requests made to download, upload and sync.
+
+    key: unique
+    destination_key: unique
+    local_path: file/directory path, filesystem root path for sync
+    remote_path: file/directory path, filesystem root path for sync
+    action: enum option upload or download
+    """
+
+    key: str = Field(
+        ...,
+        title="Source filesystem name",
+        description="Unique identifier given as the filesystem 'name' in the config file",
+    )
+    destination_key: str = Field(
+        ...,
+        title="Destination filesystem name",
+        description="Unique identifier given as the filesystem 'name' in the config file",
+    )
     local_path: str
     remote_path: str
-    action: Direction
+    action: Direction = Field(
+        ...,
+        title="Transfer direction",
+        description="Can be 'upload' or 'download for local to remote or remote to local respectively",
+    )
